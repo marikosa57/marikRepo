972535a3f62baee413f9861163a14303da04980e
marikosa57 <101106029+marikosa57@users.noreply.github.com>
2022-03-07 14:39:41 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+
+.CvDJxb {
+    min-width: 1121px;
+    z-index: 128;
+}
+user agent stylesheet
+div {
+    display: block;
+}
+@media (max-width: 1300px)
+.srp {
+    --rhs-margin: 44px;
+}
+@media (min-width: 1121px) and (max-width: 1300px)
+.srp {
+    --center-column-margin: calc((100vw - 1065px) / 2 + 0px);
+}
+.srp {
+    --center-column-margin: 180px;
+    --center-column-width: 652px;
+    --rhs-margin: 60px;
+    --rhs-width: 369px;
+}
+body {
+    margin: 0;
+    color: #bdc1c6;
+}
+body, h1 {
+    font-size: 14px;
+}
+html, body, h1, input, select {
+    font-family: arial,sans-serif;
